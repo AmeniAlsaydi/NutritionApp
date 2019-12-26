1625bd4ac280ac5ff86d4f8a6884b707bf758e38
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-12-26 18:56:57 -0500

    Initial Commit

---
+++ Nutrition/Model/Food.swift
@@ -0,0 +1,24 @@
+//
+//  Food.swift
+//  Nutrition
+//
+//  Created by Amy Alsaydi on 12/26/19.
+//  Copyright © 2019 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
+
+
+struct FoodSearch: Codable {
+    let hits: [Food]
+}
+
+struct Food: Codable {
+    let fields: Feild
+}
+
+struct Feild: Codable {
+    let item_id: String
+    let item_name: String
+}
+
---
+++ Nutrition/Networking/FoodAPI.swift
@@ -0,0 +1,9 @@
+//
+//  FoodAPI.swift
+//  Nutrition
+//
+//  Created by Amy Alsaydi on 12/26/19.
+//  Copyright © 2019 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
