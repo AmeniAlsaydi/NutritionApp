220a2e8d579b705f16e392ce5669d3c0d904fd93
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-12-30 07:24:41 -0500

    added UIViewController extension for alerts.

--- Nutrition/Controllers/NewItemViewController.swift
+++ Nutrition/Controllers/NewItemViewController.swift
@@ -14,19 +14,26 @@ class NewItemViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var createdFood = [addedFood]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-    // NOTES:
-    // The custom cell will have:
-        // - The food name
-        // - The brand
-        // - A plus sign that will then add that food item to the array of food in the addedFood object along with the name that i get from the text feild
-        // To add the item using the button on the cell I need to figure how to link it to the specific cells info??? *****
-        // I think I could calulate the number of Calories but mapping through the [Food] -> this might not work because cal info is in the FoodInfo
-        // Do I need to create another tab to show the list of the created foods the user adds? or should it be saved/posted to a seperate api?
+    
+    @IBAction func newMealButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
+// NOTES:
+// The custom cell will have:
+    // - The food name
+    // - The brand
+    // - A plus sign that will then add that food item to the array of food in the addedFood object along with the name that i get from the text feild
+    // To add the item using the button on the cell I need to figure how to link it to the specific cells info??? *****
+    // I think I could calulate the number of Calories but mapping through the [Food] -> this might not work because cal info is in the FoodInfo
+    // Do I need to create another tab to show the list of the created foods the user adds? or should it be saved/posted to a seperate api?
