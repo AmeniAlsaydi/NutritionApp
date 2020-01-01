4d9710b88b4d356a352bb8c500c7f0d35359ef3c
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-01-01 10:35:55 -0500

    added date to home vc, and created meal log vc.

--- Nutrition/Controllers/MainViewController.swift
+++ Nutrition/Controllers/MainViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
 
+    @IBOutlet weak var todaysDateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMMM d, yyyy"
+
+        let currentDate: Date = Date()
+
+        todaysDateLabel.text = dateFormatter.string(from: currentDate)
+        
+        
     }
 
 
---
+++ Nutrition/Controllers/MealLogViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MealLogViewController.swift
+//  Nutrition
+//
+//  Created by Amy Alsaydi on 1/1/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class MealLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
