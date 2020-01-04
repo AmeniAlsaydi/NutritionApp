fe917967c4aa2d9ff023db1da54fe028061336b3
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-01-03 22:02:12 -0500

    Fixed bug with add button in detailVC, search scope still not functional.

--- Nutrition/Controllers/SearchViewController.swift
+++ Nutrition/Controllers/SearchViewController.swift
@@ -26,31 +26,29 @@ class SearchViewController: UIViewController {
         }
     }
     
-    var customFoods = [addedFood]()
+    var customFoods = [addedFood]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     var currentScope = SearchScope.database
     
     var searchQuery = "" {
         didSet {
-            switch currentScope {
-            case .database:
-                DispatchQueue.main.async {
-                    self.searchFoods()
-                }
-            case .custom:
-                // here is where I have to get to the local data.
-                // i think i could leave this as is and just edit the search food to switch on scope
-                break
+            DispatchQueue.main.async {
+                self.searchFoods()
             }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
         tableView.dataSource = self
         searchBar.delegate = self
         searchFoods()
-
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -60,28 +58,22 @@ class SearchViewController: UIViewController {
         
         detailVC.selectedFood = foods[indexpath.row]
     }
-
+    
     func searchFoods() {
-        
-        switch currentScope {
-        case .database:
             FoodAPIClient.getFoodList(searchQuery: searchQuery) { (result) in
-                    switch result {
-                    case .failure(let appError):
-                        print("appError: \(appError)")
-                    case .success(let foods):
-                        DispatchQueue.main.async {
-                            self.foods = foods
-                        }
-                        
+                switch result {
+                case .failure(let appError):
+                    print("appError: \(appError)")
+                case .success(let foods):
+                    DispatchQueue.main.async {
+                        self.foods = foods
                     }
+                    
                 }
-        case .custom:
-            customFoods = NewItemViewController.createdFoods
             }
-        }
-        
-
+       
+            customFoods = NewItemViewController.createdFoods
+    }
 }
 
 extension SearchViewController: UITableViewDataSource {
@@ -97,8 +89,6 @@ extension SearchViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-       
-        
         
         switch currentScope {
         case .database:
@@ -107,44 +97,52 @@ extension SearchViewController: UITableViewDataSource {
             cell.textLabel?.text = food.fields.item_name.capitalized
             cell.detailTextLabel?.text = food.fields.brand_name
         case .custom:
+            // never comes in here even once the currentScope is custom ???? 
             let food = customFoods[indexPath.row]
-            
+
             cell.textLabel?.text = food.name
             cell.detailTextLabel?.text = food.numberOfCals?.description
         }
         
         return cell
     }
-    
-    
 }
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
-            
-            guard let searchText = searchBar.text else {
-                return
-            }
-            
-            guard !searchText.isEmpty else {
-                searchFoods()
-                return
-            }
-            
-            searchQuery = searchText.lowercased().addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? "no"
-            
+        
+        guard let searchText = searchBar.text else {
+            return
         }
+        
+        guard !searchText.isEmpty else {
+            searchFoods()
+            return
+        }
+        
+        searchQuery = searchText.lowercased().addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? "no"
+        
+    }
     
     func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
         switch selectedScope {
         case 0:
+            //print("scope: 0")
             currentScope = .database
         case 1:
+            //print("scope: 1")
             currentScope = .custom
         default:
             print("not a valid search scope")
         }
+        
     }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
+}
 
